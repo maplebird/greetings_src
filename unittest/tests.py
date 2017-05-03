b6b9b2e01b5a543a853436d25d0762c41ca90511
Vlad <vlad.dzhabarov@gmail.com>
2017-05-03 10:13:29 -0700

    Added 200 OK unit test, finalized unit test script

--- unittest/tests.py
+++ unittest/tests.py
@@ -3,6 +3,9 @@
 import requests
 import argparse
 import sys
+import json
+
+final_result = []
 
 
 def get_args():
@@ -24,15 +27,58 @@ def get_url(args):
     return url
 
 
+# Grabs content of rest API response
+def get_content(json_var):
+    response = json.loads(json_var)
+    return response['content']
+
+
+# Run all unit tests
 def run_tests(url):
-    # Test 1: GET query
-    test1 = requests.get(url)
-    print test1.content
+
+    # Test 1: Server 200 OK
+    resp = requests.get(url).status_code
+    if resp == 200:
+        print 'Test 1 successful, server is up'
+        final_result.append(1)
+    else:
+        final_result.append(0)
+        print 'Test 1 failed.  Server response:'
+        print resp
+
+    # Test 2: GET query
+    resp = requests.get(url).content
+    content = get_content(resp)
+    if 'Hello, World!' in content:
+        print 'Test 2 successful, GET returns Hello, World!'
+        final_result.append(1)
+    else:
+        final_result.append(0)
+        print 'Test 2 failed.  Server returned:'
+        print content
+    # Add new tests below
+
+
+# Show final results
+def show_results():
+    successful = 0
+    total = len(final_result)
+
+    for result in final_result:
+        successful += result
+    print "%s out of %s tests were successful." % (successful, total)
+
+    # Fail if any unit tests fail
+    if successful < total:
+        sys.exit(1)
+
 
 def main():
     args = get_args()
     url = get_url(args)
     run_tests(url)
+    show_results()
+
 
 if __name__ == "__main__":
     main()
